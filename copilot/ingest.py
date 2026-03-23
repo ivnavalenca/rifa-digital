226b8be913021bd7611063caa59a445f0cac8810
Ivna Valença <ivnavalenca@users.noreply.github.com>
2026-03-23 07:28:20 -0300

    Update ingest.py

--- copilot/ingest.py
+++ copilot/ingest.py
@@ -1,7 +1,7 @@
 import os
 
 from langchain_community.document_loaders import DirectoryLoader
-from langchain-text-splitters import CharacterTextSplitter
+from langchain_text_splitters import CharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_openai import OpenAIEmbeddings
 
